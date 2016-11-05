c5b2fafa74bdbfe98d9d2a4b23163139504435eb
Justin <Justin>
2016-11-04 18:11:59 -0700

    added ngrams relating to donations and get features

--- ParseMbox.py
+++ ParseMbox.py
@@ -1,6 +1,13 @@
 import mailbox, re, os, string, nltk
-
+from nltk.corpus import wordnet
+from nltk import ngrams
+from nltk import sent_tokenize
 arr = []
+
+donate_words = ['donate', 'donation', 'give',
+                'contribute','contribution', ['chip','in']
+                ]
+
 
 def getcharsets(msg):
     charsets = set({})
@@ -26,14 +33,32 @@ def parseDate(date):
     day_month = re.sub(' ','-', day_month)
     arr.append(day_month)
 
+def get_features(ngram, mbox):
+    donation_amount = ngram[2]
+    firstWord = ngram[0]
+    secondWord = ngram[1]
+    fourthWord = ngram[3]
+    fifthWord = ngram[4]
+    author = mbox
+
+    features = ({"first_word":firstWord,
+                 "second_word":secondWord,
+                 "fourth_word":fourthWord,
+                 "fifth_word":fifthWord,
+                 "donation_amount":donation_amount,
+                 "author:":author})
+    return features
+
 super_dict = {}
 punct = ";.?!,:()[]{}"
 exclude = set(string.punctuation)
 
+
+
 count = 0
-for mbox in os.listdir('./Mail/'):
+for mbox in os.listdir('./Mail2/'):
     print(mbox)
-    mail = mailbox.mbox('./Mail/' + mbox)
+    mail = mailbox.mbox('./Mail2/' + mbox)
     super_dict[mbox] = {}
     #mail = mailbox.mbox('./Mail/Democrats-Clinton.mbox')
 
@@ -49,24 +74,87 @@ for mbox in os.listdir('./Mail/'):
         super_dict[mbox][date]['subject'] = message['subject']
 
         msg = getBody(message)
-        #print(msg)
+
+        #For getting sentences
+        '''
+        sentArr = []
+        for sent in re.split('[?.!]', msg):
+            sent = re.sub('\n', '',sent)
+            sent = re.sub(u"\u200c",'',sent)
+            sent = re.sub(u"\u200b",'',sent)
+            sent = re.sub(r'<[^<]+?>', '',sent)
+
+            if(sent != ''):
+                sentArr.append(sent)
+        print(sentArr)
+        '''
+        '''
+        sents = sent_tokenize(msg)
+        #print(sents)
+        for sent in sents:
+            sent = re.sub('\n', '',sent)
+            sent = re.sub(u"\u200c",'',sent)
+            sent = re.sub(u"\u200b",'',sent)
+            sent = re.sub(r'<[^<]+?>', '',sent)
+            if('$' in sent and
+                   ('donate' in sent or 'give' in sent or
+                    'donation' in sent or 'giving' in sent or
+                    'chip in' in sent or 'contribute' in sent or
+                    'contribution' in sent)):
+                print(sent)
+        '''
+
         #For getting individual words
+        prev_word = ''
         for w in msg.split():
             word = re.sub('\n', '',w)
             word = re.sub(u"\u200c",'',word)
-            word = ''.join(chr for chr in word
-                           if chr not in punct)
-            if(word != ''):
+            word = re.sub(u"\u200b",'',word)
+            word = re.sub(r'<[^<]+?>', '',word)
+
+            if('$' in word):
                 words.append(word)
+            else:
+                word = ''.join(chr for chr in word
+                               if chr not in punct)
+                if(word != '' and "http" not in word):
+                    words.append(word)
+
+        sents = ' '.join(words)
+        #print(sents)
+        fivegrams = ngrams(sents.split(), 5)
+        for gram in fivegrams:
+            if('$' in gram[2] and
+                   ('donate' in gram or 'give' in gram or
+                    'donation' in gram or 'giving' in gram or
+                    'chip in' in gram or 'contribute' in gram or
+                    'contribution' in gram)):
+                print(gram)
+                get_features(gram, mbox)
 
         super_dict[mbox][date]['body'] = words
+
         #print(words)
         #print()
-        #count += 1
-#print(count)
-#print(super_dict[mbox])
+        count += 1
+        #if(count == 20):
+        #    exit(1)
+    #print(super_dict[mbox])
+    #break
+#exit(1)
+
+
+'''
+#Done with this part, look at dates.txt
 
 fdist = nltk.FreqDist(arr)
-#print(sum(fdist.values()))
+print(count)
+print(sum(fdist.values()))
+
+fp = open("dates.txt", 'w+')
+for item, count in fdist.items():
+    fp.write(item + " " + str(count) + '\n')
+fp.close()
+
 print(sorted(fdist.items(),key=lambda x: x[1]))
-#print(super_dict)
+'''
